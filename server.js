a0206f854ccc13e0c20038c3e4d5d6d0f9e49842
tiffanystud <tiffanylarsson.stud@gmail.com>
2025-04-14 17:59:03 +0200

    feat: add initial frontend layout
    Created basic page layout with sections for listing, adding, and searching cities

---
+++ server.js
@@ -0,0 +1,26 @@
+
+const cities = [
+  { id: 2, name: "Lille", country: "France"},
+  { id: 3, name: "Nantes", country: "France"},
+  { id: 5, name: "Bremen", country: "Germany"},
+  { id: 10, name: "Dresden", country: "Germany"},
+  { id: 11, name: "Heidelberg", country: "Germany"},
+  { id: 12, name: "Venice", country: "Italy"},
+  { id: 13, name: "Rome", country: "Italy"},
+  { id: 16, name: "Graz", country: "Austria"},
+  { id: 20, name: "Basel", country: "Switzerland"},
+  { id: 21, name: "Lucerne", country: "Switzerland"},
+  { id: 22, name: "Kraków", country: "Poland"},
+  { id: 23, name: "Warsaw", country: "Poland"}, 
+  { id: 24, name: "Poznań", country: "Poland"},
+  { id: 28, name: "Ghent", country: "Belgium"},
+  { id: 31, name: "Maastricht", country: "Netherlands"},
+  { id: 38, name: "Maribor", country: "Slovenia"},
+  { id: 42, name: "Strasbourg", country: "France"},
+];
+
+
+function handler (request) {
+
+
+}
